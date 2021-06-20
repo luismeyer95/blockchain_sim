5f93989bd809048b12741c7e5031bef9ead1c02b
luismeyer95 <luismeyer95@gmail.com>
2021-06-20 15:29:37 +0200

    poc progress

--- src/Transactions/AccountTransaction.ts
+++ src/Transactions/AccountTransaction.ts
@@ -1,78 +1,133 @@
 import { z } from "zod";
 
+interface IAccountOperation {
+    // public key of account
+    address: string;
+
+    // positive/negative number added to the last balance of the account
+    // representing its state change in the context of a tx
+    operation: number;
+
+    // base64 signature hash of the last tx referencing the account's balance
+    last_ref: string | null;
+
+    // newly computed balance of the account using last_ref's balance
+    updated_balance: number;
+}
+
 // regular account movement transaction
 // can be verified by stakeholders by applying verification of the signature
 // using the source acccount's public key
 interface IAccountTransaction {
-    // public keys of source and destination accounts as base64 strings
-    from: string;
-    to: string;
+    header: {
+        // base64 string representation of the tx payload hash
+        hash: string;
 
-    // self-explanatory
-    amount: number;
+        // base64 string representation of the tx payload signed
+        // with the source account private key
+        signature: string;
+    };
 
-    // unix time
-    // important: a transaction timestamp older than the last mined
-    // block's timestamp should be considered invalid
-    timestamp: string;
+    // encapsulates signable props to make it easier to hash/sign
+    payload: {
+        // operation should be - for from and + for to
+        // nodes validate that from.operation + sum(to[].operation) + miner_fee === 0
+        // source account must have a last_ref!
+        from: IAccountOperation;
+        to: IAccountOperation[];
+        miner_fee: number;
 
-    // signed hash of the result of
-    // JSON.stringify({from, to, amount, timestamp}) using the source
-    // account's private key (as a base64 string)
-    signature: string;
+        // unix time
+        // important: a transaction timestamp older than the last mined
+        // block's timestamp should be considered invalid
+        timestamp: string;
+    };
 }
 
 interface ICoinbaseTransaction {
-    // public key of miner's account
-    to: string;
-    // miner reward, will be set to 1
-    amount: number;
+    header: {
+        // base64 string representation of the coinbase payload hash
+        hash: string;
 
-    // unix time
-    // important: a transaction timestamp older than the last mined block's timestamp
-    // should be considered invalid
-    timestamp: string;
+        // base64 string representation of the tx payload signed
+        // with the MINER's private key.
+        signature: string;
+    };
 
-    // base64 signature hash of the last tx referencing the account's balance
-    // or 'none' for new accounts
-    last_ref: string;
+    payload: {
+        // operation must be block reward + sum(fees) of containing block
+        to: IAccountOperation;
 
-    // newly computed balance of the account using last_ref's balance
-    destination_balance: number;
+        // unix time
+        // important: a transaction timestamp older than the last mined block's timestamp
+        // should be considered invalid
+        timestamp: string;
+    };
 
     // miner will need to sign the block with his private key
-    // so that the coinbase destination address can be verified
+    // to prove ownership of the generated coinbase
 }
 
 interface IBlock {
-    // block's index in the blockchain
-    index: number;
+    header: {
+        // base64 string representation of the block hash.
+        // mining involves hashing the JSON.stringified payload
+        // until a certain nonce makes the hash fulfill a complexity
+        // requirement
+        hash: string;
+    };
+    // encapsulates signable props to make it easier to hash/sign
+    payload: {
+        // block's index in the blockchain
+        index: number;
 
-    // unix time
-    // important: a transaction timestamp older than the last mined block's timestamp
-    // should be considered invalid
-    timestamp: number;
+        // unix time
+        // important: a transaction timestamp older than the last mined block's timestamp
+        // should be considered invalid
+        timestamp: number;
 
-    // the magic number that makes the block hash fulfill the complexity requirement
-    nonce: number;
+        // the magic number that makes the block hash fulfill the complexity requirement
+        nonce: number;
 
-    // base64 string hash of the JSON.stringified props of the block (minus hash ofc)
-    hash: string;
-    // base64 string hash of the previous block
-    previous_hash: string;
+        // base64 string hash of the previous block
+        // or null for genesis block
+        previous_hash: string | null;
 
-    //
-    transactions: string;
-    miner_signature: string;
+        // miner's reward
+        coinbase_tx: ICoinbaseTransaction;
+
+        // array of account transactions
+        txs: IAccountTransaction[];
+    };
 }
 
+// BLOCKCHAIN BIRTH
+
 // 1. first node spawns and starts mining with newly generated keys
+
 // 2. other nodes join the network and start mining as well
+
 // 3. each node competes in the process of trying to find a gold nonce
 //    (fulfilling a certain complexity requirement)
 //    for the hash of a block containing a single coinbase transaction
-//    with the 'from' field including their own account's public key
-// 4. one node eventually finds the gold nonce, signs it with his private key
-//    and starts broadcasting it
-// 5. receiving nodes validate the block's hash with the complexity requirement
-//    along with the (block signature, coinbase 'to' public key) pair
+//    with the 'payload.to' field including their own account's public key
+
+// 4. no broadcasted transactions because no coins yet, so miners only go
+//    for the block reward and don't dynamically update their coinbase tx with tx fees
+
+// 5. one node eventually finds the gold nonce for their block, signs it their private key
+//    and starts broadcasting the block
+
+// 6. receiving nodes try to validate the block
+
+// block validation involves:
+//      - validating the block hash proof with the correct complexity by hashing
+//        the payload, checking leading zeros and checking against block.header.hash
+//      - block.payload.prev_hash === prevblock.header.hash
+//      - checking (coinbase.header.signature, coinbase.payload.to public key)
+//      - block.payload.index is an increment of the previous block's index
+//      - if current block is not genesis, make sure that the following is true =>
+//        prevblock.payload.timestamp < block.payload.timestamp < Date.now()
+//      - same goes for prevblock.payload.coinbase_tx.timestamp
+//      - validate account transactions(**) and compute total transaction fees
+//      - validate that block.coinbase.payload.to.operation
